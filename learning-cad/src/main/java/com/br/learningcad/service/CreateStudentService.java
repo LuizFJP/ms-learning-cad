e1124d19e2b712f8b05598088cdae029dfa92b35
LuizFJP <luizfjportela@gmail.com>
2023-02-14 15:04:42 -0300

    create courseNotFoundException and fetch method

--- learning-cad/src/main/java/com/br/learningcad/service/CreateStudentService.java
+++ learning-cad/src/main/java/com/br/learningcad/service/CreateStudentService.java
@@ -4,11 +4,13 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.br.learningcad.data.StudentRepository;
 import com.br.learningcad.enitity.StudentEntity;
+import com.br.learningcad.exception.CourseNotFoundException;
 
-import dto.StudentDTO;
+import dto.*;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -18,8 +20,23 @@ public class CreateStudentService {
   StudentRepository studentRepository;
 
   public UUID create(StudentDTO studentDTO) {
+    var courseDTO = fetchCourse(studentDTO.getCourseId());
+    checkCourseExists(courseDTO);
     var studentEntity = new StudentEntity(studentDTO);
     studentRepository.save(studentEntity);
     return studentEntity.getStudentId();
   }
+
+  private CourseDTO fetchCourse(UUID courseId) {
+    String uri = "http://localhost:8081/api/v1/courses/?courseId=" + courseId;
+    RestTemplate restTemplate = new RestTemplate();
+    CourseDTO response = restTemplate.getForObject(uri, CourseDTO.class);
+    return response;
+  }
+
+  private void checkCourseExists(CourseDTO courseDTO) {
+    if (courseDTO == null)
+      throw new CourseNotFoundException();
+  }
+
 }
