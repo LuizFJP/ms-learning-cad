7931f53870be07fdb0595f54ea605410f3899954
LuizFJP <luizfjportela@gmail.com>
2023-02-14 13:53:44 -0300

    add student entity

---
+++ learning-cad/src/main/java/com/br/learningcad/data/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.br.learningcad.data;
+
+public class StudentRepository {
+  
+}
---
+++ learning-cad/src/main/java/com/br/learningcad/enitity/StudentEntity.java
@@ -0,0 +1,94 @@
+package com.br.learningcad.enitity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("students")
+public class StudentEntity {
+  @Id
+  private String id;
+  private UUID studentId;
+  private String firstName;
+  private String lastName;
+  private String document;
+  private LocalDate birthDate;
+  private UUID courseId;
+  private boolean status;
+  private LocalDate createdOn;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public UUID getStudentId() {
+    return studentId;
+  }
+
+  public void setStudentId(UUID studentId) {
+    this.studentId = studentId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getDocument() {
+    return document;
+  }
+
+  public void setDocument(String document) {
+    this.document = document;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public UUID getCourseId() {
+    return courseId;
+  }
+
+  public void setCourseId(UUID courseId) {
+    this.courseId = courseId;
+  }
+
+  public boolean isStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean status) {
+    this.status = status;
+  }
+
+  public LocalDate getCreatedOn() {
+    return createdOn;
+  }
+
+  public void setCreatedOn(LocalDate createdOn) {
+    this.createdOn = createdOn;
+  }
+
+}
