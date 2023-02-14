299e7b96ad00c0264d43efd6b565b39c04470ae7
LuizFJP <luizfjportela@gmail.com>
2023-02-14 13:54:57 -0300

    create interface repository

--- learning-cad/src/main/java/com/br/learningcad/data/StudentRepository.java
+++ learning-cad/src/main/java/com/br/learningcad/data/StudentRepository.java
@@ -1,5 +1,12 @@
 package com.br.learningcad.data;
 
+import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.br.learningcad.enitity.StudentEntity;
+
 public class StudentRepository {
-  
+  public interface CourseRepository extends MongoRepository<StudentEntity, UUID> {
+  }
 }
