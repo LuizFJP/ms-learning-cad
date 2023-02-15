11d04fc5e2a958c0c7bfee2459577d5edd701822
LuizFJP <luizfjportela@gmail.com>
2023-02-14 21:54:44 -0300

    create student event dto

---
+++ learning-cad/src/main/java/com/br/learningcad/interfaces/IEventService.java
@@ -0,0 +1,5 @@
+package com.br.learningcad.interfaces;
+
+public interface IEventService {
+  public send(S)
+}
---
+++ learning-cad/src/main/java/dto/CreatedStudentEventDTO.java
@@ -0,0 +1,34 @@
+package dto;
+
+import java.util.UUID;
+
+public class CreatedStudentEventDTO {
+  private UUID studentId;
+  private String fullName;
+  private UUID courseId;
+
+  public UUID getStudentId() {
+    return studentId;
+  }
+
+  public void setStudentId(UUID studentId) {
+    this.studentId = studentId;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public UUID getCourseId() {
+    return courseId;
+  }
+
+  public void setCourseId(UUID courseId) {
+    this.courseId = courseId;
+  }
+
+}
