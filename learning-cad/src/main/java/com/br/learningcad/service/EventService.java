49fb9fd581463bae3c1ec9b770f8cb8de74555b6
LuizFJP <luizfjportela@gmail.com>
2023-02-14 22:10:59 -0300

    crate event service class

--- learning-cad/src/main/java/com/br/learningcad/service/EventService.java
+++ learning-cad/src/main/java/com/br/learningcad/service/EventService.java
@@ -3,10 +3,23 @@ package com.br.learningcad.service;
 import org.springframework.stereotype.Service;
 
 import com.br.learningcad.interfaces.IEventService;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
 import com.br.learningcad.dto.CreatedStudentEventDTO;
 
 @Service
+@RequiredArgsConstructor
 public class EventService implements IEventService {
+  @Value("${topic.student.created}")
+  private String topicName;
+
+  private final KafkaTemplate<String, CreatedStudentEventDTO> kafkaTemplate;
+
   @Override
-  public void send(CreatedStudentEventDTO createdStudentEventDTO) {}
+  public void send(CreatedStudentEventDTO createdStudentEventDTO) {
+    kafkaTemplate.send(topicName, createdStudentEventDTO);
+  }
 }
